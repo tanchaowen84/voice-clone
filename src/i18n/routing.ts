cf8b0402a9c26ed5e6bee358fd4546b5934a3f15
javayhu <javayhu@gmail.com>
2025-03-02 21:51:39 +0800

    refactor: remove German language support from i18n configuration

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "de", "zh"],
+  locales: ["en", "zh"],
   defaultLocale: "en",
   pathnames: {
     "/": "/",
