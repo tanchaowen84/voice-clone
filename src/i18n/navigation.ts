851bbabd71712b146d7616e204fb84904aaa9108
javayhu <javayhu@gmail.com>
2025-03-02 23:39:06 +0800

    feat: optimize next intl configuration & add comments

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,5 +1,10 @@
 import { createNavigation } from "next-intl/navigation";
 import { routing } from "./routing";
 
+/**
+ * Navigation APIs
+ * 
+ * https://next-intl.dev/docs/routing/navigation
+ */
 export const { Link, getPathname, redirect, usePathname, useRouter } =
   createNavigation(routing);
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,11 +1,33 @@
 import { defineRouting } from "next-intl/routing";
 
+export const LOCALES = ["en", "zh"];
+export const DEFAULT_LOCALE = "en";
+export const LOCALE_NAMES: Record<string, string> = {
+  en: "English",
+  zh: "中文",
+};
+
+/**
+ * Next.js internationalized routing
+ * 
+ * https://next-intl.dev/docs/routing
+ */
 export const routing = defineRouting({
-  locales: ["en", "zh"],
-  defaultLocale: "en",
+  // A list of all locales that are supported
+  locales: LOCALES,
+  // Default locale when no locale matches
+  defaultLocale: DEFAULT_LOCALE,
+  // Auto detect locale
+  // https://next-intl.dev/docs/routing/middleware#locale-detection
+  localeDetection: false,
+  // The prefix to use for the locale in the URL
+  // https://next-intl.dev/docs/routing#locale-prefix
+  localePrefix: "as-needed",
+  // The pathnames for each locale
+  // https://next-intl.dev/docs/routing#pathnames
   pathnames: {
     "/": "/",
-  }
+  },
 });
 
 export type Pathnames = keyof typeof routing.pathnames;
