18cbd059befd515c73ab4665c63e4e7b1c10d9e3
javayhu <javayhu@gmail.com>
2025-04-14 23:41:17 +0800

    fix: screen width bug when having posts and no post

--- src/components/shared/empty-grid.tsx
+++ src/components/shared/empty-grid.tsx
@@ -5,8 +5,10 @@ export default function EmptyGrid() {
 
   return (
     <div>
-      <div className="my-8 h-32 w-full flex items-center justify-center">
-        <p className="font-medium text-muted-foreground">{t('noPostsFound')}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="col-span-full my-8 h-32 w-full flex items-center justify-center">
+          <p className="font-medium text-muted-foreground">{t('noPostsFound')}</p>
+        </div>
       </div>
     </div>
   );
