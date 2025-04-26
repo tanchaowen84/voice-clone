625bee14ef4a258c5f2bcda47f1bd75ce619467d
javayhu <javayhu@gmail.com>
2025-04-26 10:19:44 +0800

    feat: add Ripple component and its demo example to MagicuiPage

--- src/app/[locale]/(marketing)/(pages)/magicui/page.tsx
+++ src/app/[locale]/(marketing)/(pages)/magicui/page.tsx
@@ -4,6 +4,7 @@ import { DotPatternDemo } from '@/components/magicui/example/dot-pattern-example
 import { GridPatternDemo } from '@/components/magicui/example/grid-pattern-example';
 import { HeroVideoDialogDemoTopInBottomOut } from '@/components/magicui/example/hero-video-dialog-example';
 import { MarqueeDemoVertical } from '@/components/magicui/example/marquee-example';
+import { RippleDemo } from '@/components/magicui/example/ripple-example';
 
 /**
  * Magic UI Components Showcase Page
@@ -15,9 +16,12 @@ export default async function MagicuiPage() {
     <div className="mx-auto space-y-8">
       <DotPatternDemo />
       <GridPatternDemo />
+      <RippleDemo />
       <BentoDemo />
-      <MarqueeDemoVertical />
-      <AnimatedListDemo />
+      <div className="grid md:grid-cols-2 gap-4">
+        <MarqueeDemoVertical />
+        <AnimatedListDemo />
+      </div>
       <HeroVideoDialogDemoTopInBottomOut />
     </div>
   );
